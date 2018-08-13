50d14e5e1da66486881e61b4fad3e3b50c7f7518
JSSRDRG <39565615+JSSRDRG@users.noreply.github.com>
2018-08-13 17:39:54 +0200

    improvements

--- assets/js/com.js
+++ assets/js/com.js
@@ -7,13 +7,13 @@ const port = new SerialPort('COM11', {
 port.on('open', onOpen)
 port.on('data', onData)
 
-function onOpen() {
-	byId('mcuLinkStatus').classList.remove('mcuLinkStatusDisconnected')
-	byId('mcuLinkStatus').classList.add('mcuLinkStatusLink')
-}
+	function onOpen() {
+		byId('mcuLinkStatus').classList.remove('mcuLinkStatusDisconnected')
+		byId('mcuLinkStatus').classList.add('mcuLinkStatusLink')
+	}
 
-function onData(data) {
-	console.log(data)
-	const dataView = byId('data')
-	dataView.innerHTML = data
-}
+	function onData(data) {
+		console.log(data)
+		const dataView = byId('data')
+		dataView.innerHTML = data
+	}
