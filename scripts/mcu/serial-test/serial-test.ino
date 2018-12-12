08100a8e8fea7be722d9b112ba6ec22eb39179be
JSSRDRG <39565615+JSSRDRG@users.noreply.github.com>
2018-12-12 10:44:27 +0100

    Added function test

--- scripts/mcu/serial-test/serial-test.ino
+++ scripts/mcu/serial-test/serial-test.ino
@@ -2,13 +2,14 @@ int x = 0;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  Serial.print("{FNC:sett0;}");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.print("{GPS:");
   Serial.print(x);
-  Serial.print(";AcX:12,33;}");
+  Serial.print(";AcX:12,33;");
   x += 1;
   delay(200);
 }
