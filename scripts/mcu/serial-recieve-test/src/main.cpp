e8f589e9d01a46c44dbf66dec4615d6fb20cd9a1
Jesse Rodrigo <39565615+JSSRDRG@users.noreply.github.com>
2019-01-26 22:22:20 +0100

    Update test script

--- scripts/mcu/serial-recieve-test/src/main.cpp
+++ scripts/mcu/serial-recieve-test/src/main.cpp
@@ -1,17 +1,81 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <RH_RF95.h>
 
-int x = 0;
+#define RFM95_RST 9
+#define RFM95_CS 4
+#define RFM95_INT 3
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 868.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+// Blinky on receipt
+#define LED 13
 
 void setup() {
-  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(1);
+  }
+  delay(100);
+
+  Serial.println("Feather LoRa RX Test!");
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+
+  rf95.setTxPower(23, false);
 }
 
 void loop() {
-  Serial.print("{GPS:");
-  Serial.print(x);
-  Serial.print(";AcX:12,33;AcY:");
-  Serial.print(rand() % 5);
-  Serial.print(";}");
-  x += 1;
-  delay(200);
+  if (rf95.available())
+  {
+    // Should be a message for us now
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+
+    if (rf95.recv(buf, &len))
+    {
+      digitalWrite(LED, HIGH);
+      RH_RF95::printBuffer("Received: ", buf, len);
+      Serial.print("Got: ");
+      Serial.println((char*)buf);
+       Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);
+
+      // Send a reply
+      uint8_t data[] = "And hello back to you";
+      rf95.send(data, sizeof(data));
+      rf95.waitPacketSent();
+      Serial.println("Sent a reply");
+      digitalWrite(LED, LOW);
+    }
+    else
+    {
+      Serial.println("Receive failed");
+    }
+  }
 }
