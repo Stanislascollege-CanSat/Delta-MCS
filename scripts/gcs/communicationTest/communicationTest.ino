f95d8e4ba40730234de6e4a6b93be7f1cdaa2077
JSSRDRG <39565615+JSSRDRG@users.noreply.github.com>
2019-02-19 12:03:43 +0100

    Initial commit

---
+++ scripts/gcs/communicationTest/communicationTest.ino
@@ -0,0 +1,25 @@
+/*
+  @author JSSRDRG
+  GCS communicationTest
+
+*/
+
+// Inlude libraries.
+#include <SPI.h>
+#include <RH_RF95.h>
+
+// Define pin connections.
+#define RFM95_CS 4
+#define RFM95_RST 2
+#define RFM95_IRQ 3 // Must be hardware interrupt.
+
+// Define variable.
+#define RF95_FREQ 868.0 // Change after decimal for channels.
+
+void setup() {
+
+}
+
+void loop() {
+
+}
